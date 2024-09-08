6cd278a1cdc5cfc41c9a6215174724def1141072
Abhinav Tyagi <abhinav.t@zoom.us>
2024-09-08 13:44:11 +0530

    min swap needed to sort array

---
+++ src/main/java/org/example/Extra/Questions/MinSwaps.java
@@ -0,0 +1,34 @@
+package org.example.Extra.Questions;
+
+import java.util.Arrays;
+
+public class MinSwaps {
+    public int minSwaps(int nums[]) {
+        int n=nums.length;
+        int swapNeeded=0;
+        int[][] combinedNums = new int[n][2];
+        for(int i=0;i<n;i++) {
+            combinedNums[i][0]=nums[i];
+            combinedNums[i][1]=i;
+        }
+        Arrays.sort(combinedNums,(a, b)->a[0]-b[0]);
+        boolean[] visited = new boolean[n];
+
+        for(int i=0;i<n;i++) {
+            if(visited[i] || combinedNums[i][1]==i) {
+                continue;
+            }
+
+            int cycleLength=0;
+            int j=i;
+            while(!visited[j]) {
+                visited[j]=true;
+                j=combinedNums[j][1];
+                cycleLength++;
+            }
+            swapNeeded+=(cycleLength-1);
+        }
+
+        return swapNeeded;
+    }
+}
