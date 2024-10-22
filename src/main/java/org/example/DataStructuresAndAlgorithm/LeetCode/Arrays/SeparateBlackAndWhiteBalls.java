57417501e3a384b9c6da390745851a1e73aa4964
Abhinav Tyagi <abhinav.t@zoom.us>
2024-10-22 08:33:05 +0530

    added leetcode 2938. Separate Black and White Balls

---
+++ src/main/java/org/example/DataStructuresAndAlgorithm/LeetCode/Arrays/SeparateBlackAndWhiteBalls.java
@@ -0,0 +1,36 @@
+package org.example.DataStructuresAndAlgorithm.LeetCode.Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//https://leetcode.com/problems/separate-black-and-white-balls/description/
+public class SeparateBlackAndWhiteBalls {
+    public long minimumSteps(String s) {
+        int n = s.length();
+
+        //store all the ones index
+        List<Integer> onesIndex = new ArrayList<>();
+
+        for(int i=0;i<n;i++) {
+            if(s.charAt(i)=='1') {
+                onesIndex.add(i);
+            }
+        }
+
+        //sort the indices
+        Collections.sort(onesIndex, (a, b)->b-a);
+
+        long operationsNeeded=0L;
+        int lastOneIndex=n-1;
+
+        //at each ones index we have to place, it to the end, so operations needed will be
+        //lastOneIndex-currentOneIndex;
+        for(int currentOneIndex:onesIndex) {
+            operationsNeeded+=lastOneIndex-currentOneIndex;
+            lastOneIndex--;
+        }
+
+        return operationsNeeded;
+    }
+}
