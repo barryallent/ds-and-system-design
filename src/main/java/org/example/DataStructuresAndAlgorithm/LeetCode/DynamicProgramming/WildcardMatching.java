11a2ed9e9b530ee34875871c7fe725b2fed36b8f
Abhinav Tyagi <abhinav31796@gmail.com>
2025-12-30 19:54:20 +0530

    Added leetcode 44. Wildcard Matching

---
+++ src/main/java/org/example/DataStructuresAndAlgorithm/LeetCode/DynamicProgramming/WildcardMatching.java
@@ -0,0 +1,56 @@
+package org.example.DataStructuresAndAlgorithm.LeetCode.DynamicProgramming;
+
+//https://leetcode.com/problems/wildcard-matching/description/
+public class WildcardMatching {
+        Boolean[][] dp;
+        boolean isMatch(int i, int j, int n1, int n2, String s, String p) {
+
+            //base case when both reach end successfully
+            if(i==n1 && j==n2) {
+                return true;
+            }
+
+            //if j is finish but s has characters left,
+            //please note that if s="abcxxx" and p="abc" then false is expected and not true
+            //because as per question the matching should cover the entire string and not partial
+            if(j>=n2) {
+                return false;
+            }
+
+            //if 1st string is finished but 2nd is still left
+            //then we return true of remaining all are * in p else false
+            if(i>=n1) {
+                for (int k = j; k < p.length(); k++) {
+                    if (p.charAt(k) != '*') return false;
+                }
+                return true;
+            }
+
+            if(dp[i][j]!=null) {
+                return dp[i][j];
+            }
+
+            //chars match or we have a ? so increase both
+            if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?') {
+                return dp[i][j] = isMatch(i+1, j+1, n1, n2, s, p);
+            }
+
+            //wildcard matching, 2 cases,
+            //case 1: ignore the wildcard and move to next char eg->s="abcdef" p="abc*def"
+            //case 2: match the ith char and move i+1 but keep j at wildcard only so that
+            //we can match next char in future, so we are giving option for wildcard to match 1 char
+            //2 char, 3 char etc by keeping j same and i increase by 1, so no for loop is needed
+            //i think this is main crux of the problem.
+            else if(s.charAt(i)!=p.charAt(j) && p.charAt(j)=='*') {
+                return dp[i][j] = isMatch(i, j+1, n1, n2, s, p) || isMatch(i+1, j, n1, n2, s, p);
+            }
+            return dp[i][j] =  false;
+        }
+
+        public boolean isMatch(String s, String p) {
+            int n1 = s.length();
+            int n2 = p.length();
+            dp=new Boolean[n1+1][n2+1];
+            return isMatch(0, 0, n1, n2, s, p);
+        }
+}
