e99d3f92366ca924bca9badcb9be4bb3537b17c1
Abhinav Tyagi <abhinav31796@gmail.com>
2026-01-16 21:44:14 +0530

    Added leetcode Medium 173. Binary Search Tree Iterator

---
+++ src/main/java/org/example/DataStructuresAndAlgorithm/LeetCode/Trees/BinarySearchTreeIterator.java
@@ -0,0 +1,26 @@
+package org.example.DataStructuresAndAlgorithm.LeetCode.Trees;
+
+import java.util.Stack;
+
+public class BinarySearchTreeIterator {
+    Stack<TreeNode> s1 = new Stack<>();
+    TreeNode current;
+
+    public BSTIterator(TreeNode root) {
+        current = root;
+    }
+
+    public int next() {
+        while(current!=null) {
+            s1.add(current);
+            current=current.left;
+        }
+        TreeNode topElement = s1.pop();
+        current=topElement.right;
+        return topElement.val;
+    }
+
+    public boolean hasNext() {
+        return current!=null || !s1.isEmpty();
+    }
+}
